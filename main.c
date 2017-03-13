d2c7cf13f432e362b6076132d47d33e6da136cce
mashruravi <mashru.ravi@gmail.com>
2017-03-13 21:37:33 +0530

    Check for correct cmd line arguments

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
-	
-	return 0;
+void printUseError() {
+    printf("No source file specified.\n");
+    printf("Usage: cumpiler path/to/source/file\n");
+}
+
+int main(int argc, char *argv[]) {
+
+    switch (argc) {
+
+		// No file path specified
+		case 1: {
+			printUseError();
+			break;
+		}
+
+		// Correct number of arguments
+		case 2: {
+			printf("Correct Usage!");
+			break;
+		}
+
+		// More than one argument
+		default: {
+			printUseError();
+		}
+
+    }
+
+    return 0;
 }
